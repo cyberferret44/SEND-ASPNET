b2e420afed0d2a23055b37c7ddbc5bc5d66a447c
Chase Violet <ccviol01@gmail.com>
2018-05-10 11:31:28 -0700

    working maps and chat room

---
+++ Alpha/Alpha/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Alpha.Startup))]
+
+namespace Alpha
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
